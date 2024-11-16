4b4e2dbed920bb92a7be0f7ee77a11a668bc0679
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-11-16 10:32:38 +0530

    hello world in laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ar lab</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
+     @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-900 text-white">
+    <div class="flex flex-col items-center justify-center min-h-screen">
+        <h1 class="text-3xl bg-blue-500 p-4 rounded-xl">Welcome to Hello world!</h1>
+    </div>
+</body>
+
+</html>
